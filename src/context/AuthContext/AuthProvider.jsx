1ed627da77080a91cda77f089190f03f64c59f51
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-12-17 07:03:00 +0600

    logout token removed implemented and token set on onAuthStateChanged for whoe applications

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AuthContext from './AuthContext';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import auth from '../../firebase/firebase.init';
+import axios from 'axios';
 
 const AuthProvider = ({children}) => {
 	const [user, setUser] = useState(null);
@@ -20,8 +21,26 @@ const AuthProvider = ({children}) => {
 
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth , currentUser => {
-			console.log(currentUser)
+			console.log("state captured",currentUser)
 			setUser(currentUser)
+			if(currentUser?.email){
+				const user = {email: currentUser?.email}
+				axios.post('http://localhost:3000/jwt',user,{
+					withCredentials: true
+				})
+				.then(res => {
+					console.log("login token",res.data)
+					setLoading(false)
+				})
+			} else{
+				axios.post('http://localhost:3000/logout', {},{withCredentials: true})
+				.then(res => {
+					console.log("logout token",res.data)
+					setLoading(false)
+				})
+			}
+
+			
 			setLoading(false)
 		})
 		return () => {
